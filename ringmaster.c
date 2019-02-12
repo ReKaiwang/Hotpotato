916cbc03961239231b3285055749d12071a258f8
kw283 <kw283@duke.edu>
2019-02-12 17:59:07 -0500

    first line

--- ringmaster.c
+++ ringmaster.c
@@ -3,3 +3,15 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+struct _potato
+{
+	unsigned hops;
+	int * player_ID;
+};
+typedef struct _potato potato;
+int main(int argc, char *argv[]) {
+	int * socket_fd;
+	struct addrinfo host_info;
+  	struct addrinfo *host_info_list;
+
+}

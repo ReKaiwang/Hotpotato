b63bf43c38c06019ed55811d822ae13c20071f5a
kw283 <kw283@duke.edu>
2019-02-17 20:57:11 -0500

    local potato

--- player.cpp
+++ player.cpp
@@ -17,21 +17,27 @@ void *get_in_addr(struct sockaddr *sa)
   return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 int main(int argc, char *argv[]) {
-
+  //socket_fd use to connect with server and listen_fd use to listen
   int socket_fd, listen_fd;
   //int neighb_fd[2];
-  int neigh_fd[2];
-  int playernum;
-  int status;
-  struct addrinfo host_info, listen_info;
+  int neigh_fd[2]; //
+  int playernum[2]; // player number of client
+  int status; //check error status
+  struct addrinfo host_info, listen_info; // hits
   struct addrinfo *host_info_list, *listen_info_list;
-  struct sockaddr_storage * neigh_inf= new struct sockaddr_storage[3];
-  struct sockaddr_storage * neigh_add= new struct sockaddr_storage[2];
+  struct sockaddr_storage * neigh_inf= new struct sockaddr_storage[3]; // store neighbor listen socket
+  struct sockaddr_storage * neigh_add= new struct sockaddr_storage[2]; //
   const char *hostname = argv[1];
   const char *port     = argv[2];
+  const char * end = "endgame";
+  //bound port address
   struct sockaddr *listen_addr = new struct sockaddr;
+  //message to server
   char msg[256];
+
+  // make an empty potato
   potato mypotato;
+
   //set for select
   fd_set master;
   fd_set read_fds;
@@ -39,8 +45,6 @@ int main(int argc, char *argv[]) {
   FD_ZERO(&master);    // clear the master and temp sets
   FD_ZERO(&read_fds);
 
-  //string msg = "connection with ringmaster";
- // char msg[512];
   if (argc != 3) {
       printf("Syntax: client %s\n", hostname);
       return EXIT_FAILURE;
@@ -51,6 +55,7 @@ int main(int argc, char *argv[]) {
   host_info.ai_family   = AF_INET;
   host_info.ai_socktype = SOCK_STREAM;
 
+  //set for listen port
   listen_info.ai_family   = AF_INET;
   listen_info.ai_socktype = SOCK_STREAM;
   listen_info.ai_flags    = AI_PASSIVE;
@@ -60,6 +65,7 @@ int main(int argc, char *argv[]) {
         perror("Error: cannot get address info for listen\n");
         return EXIT_FAILURE;
     }
+
   //get the address for connection with ringmaster
   status = getaddrinfo(hostname, port, &host_info, &host_info_list);
   if (status != 0) {
@@ -70,21 +76,32 @@ int main(int argc, char *argv[]) {
   socket_fd  = socket(host_info_list->ai_family,
                       host_info_list->ai_socktype,
                       host_info_list->ai_protocol);
+  if (socket_fd == -1) {
+        cerr << "Error: cannot create socket" << endl;
+        cerr << "  (" << hostname << "," << port << ")" << endl;
+        return EXIT_FAILURE;
+  }
+
   fdmax = socket_fd;
   FD_SET(socket_fd, &master);
+
+  // make a socket for listenning
   listen_fd = socket(listen_info_list->ai_family,
                      listen_info_list->ai_socktype,
                      listen_info_list->ai_protocol);
-  FD_SET(listen_fd, &master);
-  if(listen_fd > fdmax)
-      fdmax = listen_fd;
-  // make listen port arbitary
-    ( (struct sockaddr_in *)listen_info_list->ai_addr)->sin_port = 0;
-  if (socket_fd == -1) {
-    cerr << "Error: cannot create socket" << endl;
-    cerr << "  (" << hostname << "," << port << ")" << endl;
-    return EXIT_FAILURE;
+
+  if (listen_fd == -1) {
+        cerr << "Error: cannot create socket" << endl;
+        cerr << "  (" << hostname << "," << port << ")" << endl;
+        return EXIT_FAILURE;
   }
+
+//  FD_SET(listen_fd, &master);
+//  if(listen_fd > fdmax)
+//      fdmax = listen_fd;
+
+  // make listen port arbitary
+  ( (struct sockaddr_in *)listen_info_list->ai_addr)->sin_port = 0;
 
   // bind to port
   status = bind(listen_fd, listen_info_list->ai_addr, listen_info_list->ai_addrlen);
@@ -101,6 +118,7 @@ int main(int argc, char *argv[]) {
         // 	cerr << "  (" << hostname << "," << port << ")" << endl;
         return -1;
     } //if
+    //get the listen socket address and port
     status  = getsockname(listen_fd,listen_addr,& listen_info_list->ai_addrlen);
     if(status == -1){
         cerr << "fail to get listen socket name" << endl;
@@ -113,31 +131,35 @@ int main(int argc, char *argv[]) {
     cerr << "fail to connect with ringmaster" << endl;
     return EXIT_FAILURE;
   }
+  // send address and port information to ringmaster
   status = send(socket_fd,listen_addr,(size_t)listen_info_list->ai_addrlen,0 );
+  // receive neighbour information from ringmaster
   status = recv(socket_fd, neigh_inf, 3*sizeof(* neigh_inf), 0);
-
   if(status == -1){
     cerr << "fail to receive message"<<endl;
     return EXIT_FAILURE;
   }
-  status = recv(socket_fd,&playernum , sizeof(int), 0);
-  cout << playernum<< endl;
+
+  //receive player number from ringmaster
+  status = recv(socket_fd,playernum , sizeof(playernum), 0);
     if(status == -1){
         cerr << "fail to receive message"<<endl;
         return EXIT_FAILURE;
     }
-  for(int i =0; i < 3; i++) {
-    struct sockaddr_in sa = *(struct sockaddr_in *) (neigh_inf+i);
-    char ip4[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(sa.sin_addr), ip4, INET_ADDRSTRLEN);
-    cout << ip4 << "" << sa.sin_port << endl;
-  }
+  cout <<"Connected as player "<<playernum[0]<<" of "<<playernum[1]<<" total players"<<endl;
+//  for(int i =0; i < 3; i++) {
+//    struct sockaddr_in sa = *(struct sockaddr_in *) (neigh_inf+i);
+//    char ip4[INET_ADDRSTRLEN];
+//    inet_ntop(AF_INET, &(sa.sin_addr), ip4, INET_ADDRSTRLEN);
+//    cout << ip4 << "" << sa.sin_port << endl;
+//  }
+
   // connect with neighb
   neigh_fd[0] = socket(listen_info_list->ai_family,
                        listen_info_list->ai_socktype,
                        listen_info_list->ai_protocol);
   socklen_t addr_size =  sizeof(*listen_addr);
-  if(playernum == 0){
+  if(playernum[0] == 0){
       status = connect(neigh_fd[0],(struct sockaddr * )neigh_inf, addr_size);
       if(status == -1){
           cerr << "fail to connect with neigh" << endl;
@@ -169,11 +191,12 @@ int main(int argc, char *argv[]) {
     if(neigh_fd[1] > fdmax)
         fdmax = neigh_fd[1];
 
-      status = send(socket_fd, "ready to listen", 20, 0);
+    status = send(socket_fd, "ready to play game", 20, 0);
       if (status == -1) {
           cerr << "fail to send listen message" << endl;
           return EXIT_FAILURE;
       }
+
       memset(msg,0,sizeof(msg)/sizeof(char));
       status = recv(socket_fd,msg,sizeof(msg)/ sizeof(char),0);
         if (status == -1) {
@@ -182,14 +205,14 @@ int main(int argc, char *argv[]) {
         }
 
       //wait for potato
-    srand( (unsigned int) time(NULL));
+    srand( (unsigned int) time(NULL)+playernum[0]);
       while(1){
           read_fds = master;
           if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
               perror("select");
               exit(4);
           }
-          cout<<"after select"<<endl;
+          //cout<<"after select"<<endl;
           for(int i =0; i <= fdmax; i++){
               if(FD_ISSET(i, & read_fds)){
                 if((status = recv(i, &mypotato, sizeof(mypotato),0))!= sizeof(mypotato))
@@ -198,17 +221,30 @@ int main(int argc, char *argv[]) {
                         cout << "socket" << i << " hung up" << endl;
                     else if(status <0)
                         cerr << "receive error from " << i << endl;
-                    else
-                        cout << "not potato message!"<<endl;
+                    else {
+                        if(status == sizeof(end)/ sizeof(char))
+                        {
+                            freeaddrinfo(host_info_list);
+                            freeaddrinfo(listen_info_list);
+                            close(socket_fd);
+                            close(listen_fd);
+                            for(int j=0; j < 2; j++){
+                                close(neigh_fd[j]);
+                            }
+                            return EXIT_SUCCESS;
+                        }
+                        else
+                        cout << "not potato message!" << endl;
+                    }
                     close(i);
                     FD_CLR(i, & read_fds);
                     return EXIT_FAILURE;
                   }
                 else{
                     mypotato.hops--;
-                    mypotato.players_ID[mypotato.already] = playernum;
+                    mypotato.players_ID[mypotato.already] = playernum[0];
                     mypotato.already++;
-                    cout << mypotato.players_ID[mypotato.already-1]<<endl;
+                    //cout << mypotato.players_ID[mypotato.already-1]<<endl;
                     if(mypotato.hops <= 0){
                         if((status = send(socket_fd, &mypotato, sizeof(mypotato),0 ))==-1){
                             cerr<<"fail to return potota to ringmaster";
@@ -216,8 +252,16 @@ int main(int argc, char *argv[]) {
                         }
                     }else{
                         int next_player = rand()%2;
-                        cout << "next player " << next_player<<endl;
-                        cout << neigh_fd[next_player]<<endl;
+                        int next_num ;
+                        if(next_player == 0)
+                            next_num = playernum[0] - 1;
+                        else
+                            next_num = playernum[0] +1 ;
+                        if(next_num < 0)
+                            next_num = playernum[1] - 1;
+                        else if (next_num >= playernum[1])
+                            next_num = 0;
+                        cout << "Sending potato to " << next_num<<endl;
                         if((status = send(neigh_fd[next_player],&mypotato, sizeof(mypotato),0 ))==-1){
                             cerr<<"fail to return potota to ringmaster";
                             return EXIT_FAILURE;
--- ringmaster.cpp
+++ ringmaster.cpp
@@ -32,7 +32,16 @@ void *get_in_addr(struct sockaddr *sa)
 
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
-
+void sendmsg(void* msg, int * socket_fd, int num, size_t size){
+    int status;
+    for(int i =0 ; i < num ; i++){
+        status = send(socket_fd[i], msg, size, 0);
+        if(status == -1 ){
+            cerr << "fail to send message to player " << i << endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+}
 int main(int argc, char *argv[]) {
 	int socket_fd; // ringmaster socket for listening client
 	int num_players = atoi(argv[2]);
@@ -45,11 +54,11 @@ int main(int argc, char *argv[]) {
   	// store the socket client uses to listen
   	struct sockaddr_storage * player_listen = new struct sockaddr_storage [num_players];
   	char buf[256];
-  	//check error status
-  	int status;
+  	int status; //check error status
   	fd_set master; //master  fd
 	fd_set read_fds; // temperate read list
   	int fdmax=0; // maximum file descriptor number
+    const char * end = "endgame";
 
   	// make a potato for the game
   	potato mypotato = {num_hops,{0},0};
@@ -91,17 +100,17 @@ int main(int argc, char *argv[]) {
    	 cerr << "Error: cannot bind socket" << endl;
    	 return EXIT_FAILURE;
   	}
-  	
+
  	 status = listen(socket_fd, 100);
   	if (status == -1) {
-    	cerr << "Error: cannot listen on socket" << endl; 
-   // 	cerr << "  (" << hostname << "," << port << ")" << endl;
-    	return -1;
+    	cerr << "Error: cannot listen on socket" << endl;
+    	return EXIT_FAILURE;
   	} //if
 	//FD_SET(socket_fd, &master);
 	//fdmax = socket_fd;
-  	cout << "Waiting for connection"<<endl;
+  	//cout << "Waiting for connection"<<endl;
 	socklen_t addr_size =  sizeof(*player_addr);
+  	// connect with client
   	for(int i = 0; i < num_players; i++){
 		int new_fd = accept(socket_fd, (struct sockaddr * ) (player_addr+i), & addr_size);
 		if(new_fd == -1){
@@ -118,16 +127,14 @@ int main(int argc, char *argv[]) {
             cerr << "fail to receive message"<<endl;
             return EXIT_FAILURE;
         }
-//		struct sockaddr_in sa =  * (struct sockaddr_in * ) (player_addr+i);
-//		char ip4[INET_ADDRSTRLEN];
-////		void * test = get_in_addr((struct sockaddr * ) (player_addr+i));
-//		inet_ntop(AF_INET, &(sa.sin_addr), ip4, INET_ADDRSTRLEN);
-//		cout << ip4 << ""<< sa.sin_port<<endl;
-		cout << "Successful connection with " << (i+1) << " player of " << num_players << endl;
+
+		cout << "Player " << i << " is ready to play" << endl;
   	}
-  	// send neighbor info to each player
+
+  	// server sends neighbor info to each player
 	for(int i = 0; i < num_players; i++){
 		struct sockaddr_storage *neigh_addr = new sockaddr_storage[3];
+		int player_num[2] = {i, num_players};
 		if(i == 0) {
 			neigh_addr[0] = player_listen[num_players-1];
 			neigh_addr[1] = player_listen[i+1];
@@ -146,14 +153,19 @@ int main(int argc, char *argv[]) {
 //			inet_ntop(AF_INET, &(sa.sin_addr), ip4, INET_ADDRSTRLEN);
 //			cout << ip4 << "" << sa.sin_port << endl;
 //		}
-		status = send(player_socketfd[i], neigh_addr , 3*sizeof(*(neigh_addr)), 0);
-		if(status == -1 ){
-			cerr << "fail to send message to player " << i << endl;
-			return EXIT_FAILURE;
-		}
-		status = send(player_socketfd[i],& i, sizeof(int),0);
+        // send neighbor information to client
+//		status = send(player_socketfd[i], neigh_addr , 3*sizeof(*(neigh_addr)), 0);
+//		if(status == -1 ){
+//			cerr << "fail to send message to player " << i << endl;
+//			return EXIT_FAILURE;
+//		}
+        sendmsg((void*)neigh_addr, player_socketfd+i, 1, 3*sizeof(*(neigh_addr)));
+		// send player number to client
+		sendmsg((void*)player_num, player_socketfd+i, 1,sizeof(player_num));
+		//status = send(player_socketfd[i],& i, sizeof(int),0);
 	}
-	int messg_num = 0;
+
+	int ready_num = 0;
 	// ready for connection to neigh
 //	for(int i = 0 ; i < num_players; i++) {
 //	    cout<<player_socketfd[i]<<endl;
@@ -169,17 +181,15 @@ int main(int argc, char *argv[]) {
 //    }
 	while(1){
 		read_fds = master;
-		//cout<<fdmax<<endl;
-		//cout<<fdmax<<endl;
-		cout << "before select"<<endl;
+		//cout << "before select"<<endl;
         if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
             perror("select");
             exit(4);
         }
-        cout<< "now start select"<<endl;
+        //cout<< "now start select"<<endl;
 		for(int i =0; i <= fdmax; i++){
 			if(FD_ISSET(i, & read_fds)){
-			    cout<<i<<endl;
+			    //cout<<i<<endl;
 				if((status = recv(i, buf, sizeof(buf),0))<=0){
 					if(status==0) {
 						cout << "socket" << i << " hung up" << endl;
@@ -191,14 +201,15 @@ int main(int argc, char *argv[]) {
 					FD_CLR(i, & read_fds);
 					return EXIT_FAILURE;
 				}// ENDOF error check
-			messg_num++;
-			cout<<buf<<endl;
+			ready_num++;
+			//cout<<buf<<endl;
 			memset(buf,0,sizeof(buf)/sizeof(char));
 			}
 		}
-		if(messg_num == num_players)
+		if(ready_num == num_players)
 			break;
 	}
+	//send beginning game to players
     for(int i = 0 ; i < num_players; i++) {
 	   // cout<<player_socketfd[i]<<endl;
         if ((status = send(player_socketfd[i], "Ready to start the game", 40, 0)) <= 0) {
@@ -210,6 +221,7 @@ int main(int argc, char *argv[]) {
         }
 
     }
+    //
 	srand( (unsigned int) time(NULL));
     int first_player = rand()% num_players;
 	cout << "Ready to start the game, sending potato to player "<<(first_player)<<endl;
@@ -217,12 +229,10 @@ int main(int argc, char *argv[]) {
     	cerr<<"fail to send potato to "<<(first_player)<<endl;
 		return EXIT_FAILURE;
     }
-    cout << "wait for potato"<<endl;
+    //cout << "wait for potato"<<endl;
+    // wait for potato
     while(1){
 		read_fds = master;
-		//cout<<fdmax<<endl;
-		//cout<<fdmax<<endl;
-		//cout << "before select"<<endl;
 		if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
 			perror("select");
 			exit(4);
@@ -230,7 +240,6 @@ int main(int argc, char *argv[]) {
 		//cout<< "now start select"<<endl;
 		for(int i =0; i <= fdmax; i++){
 			if(FD_ISSET(i, & read_fds)){
-				cout<<i<<endl;
 				if((status = recv(i, &mypotato, sizeof(mypotato),0))!= sizeof(mypotato)){
 					if(status==0) {
 						cout << "socket" << i << " hung up" << endl;
@@ -245,16 +254,27 @@ int main(int argc, char *argv[]) {
 					return EXIT_FAILURE;
 				}// ENDOF error check
 				else{
+				    // display the trace
+				    for(int i = 0; i < num_hops; i++){
+				        if(i == 0) {
+				            cout << "Trace of potato:"<<endl;
+                            cout << mypotato.players_ID[i];
+                        }
+				        else
+				            cout<<", "<<mypotato.players_ID[i];
+				    }
+				    cout<<""<<endl;
 					cout << "I'm it!"<<endl;
+					sendmsg((void*)end,player_socketfd,num_players,sizeof(end));
+                    freeaddrinfo(host_info_list);
+                    close(socket_fd);
+                    for(int j = 0; j < num_players; j++){
+                        close(player_socketfd[j]);
+                    }
 					return EXIT_SUCCESS;
 				}
 			}
 		}
     }
-	freeaddrinfo(host_info_list);
-	close(socket_fd);
-	for(int i = 0; i < num_players; i++){
-		close(player_socketfd[i]);
-	}
 	return EXIT_SUCCESS;
 }
